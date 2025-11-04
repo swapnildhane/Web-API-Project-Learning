8cd0b5b20dcff302550daa65cbbc3b13afc611e6
New Project <swapnildhane98@gmail.com>
2025-11-04 13:17:32 +0530

    New Web API Project

---
+++ HelloWorldAPI/Controllers/HelloWorldAPIController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldAPIController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Hello World from .Net Core API");
+        }
+    }
+}
